3b0090a436448b103b311294547b6861c4e12a76
leethn <leethn@gmail.com>
2017-02-08 12:32:27 +0900

    Part 3.1 진행중

--- pair_project/blog/views.py
+++ pair_project/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    context = {'post_title': Post.objects.order_by('-published_date')}
+    print(context)
+    return render(request, 'blog/index.html', context)
