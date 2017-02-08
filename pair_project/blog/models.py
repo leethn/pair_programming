871695efe186b4a4d255e2e9859c87261b168f0c
leethn <leethn@gmail.com>
2017-02-08 11:40:14 +0900

    create DB

--- pair_project/blog/models.py
+++ pair_project/blog/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
 # Create your models here.
